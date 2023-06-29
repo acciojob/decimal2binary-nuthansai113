71c354d893cd20d156149d664a309851102c69a8
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-06-29 09:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
-module.exports = threeSum;
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33)); // Output: 100001
